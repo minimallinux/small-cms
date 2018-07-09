9d6bf01284be5b53af27d604d6311bbfd5379f0a
minimallinux <anthonymcgowan1@gmail.com>
2018-07-09 23:23:15 +0100

    First Commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+ini_set( "display_errors", true );
+date_default_timezone_set( "Europe/London" );  // http://www.php.net/manual/en/timezones.php
+define( "DB_DSN", "mysql:host=localhost;dbname=webmobap_staging8" );
+define( "DB_USERNAME", "webmobap_admin" );
+define( "DB_PASSWORD", ';%"p[]7A#qa_H^+!-9:u4}}3W' );
+define( "CLASS_PATH", "classes" );
+define( "TEMPLATE_PATH", "templates" );
+define( "HOMEPAGE_NUM_ARTICLES", 5 );
+define( "ADMIN_USERNAME", "admin" );
+define( "ADMIN_PASSWORD", "mypass" );
+require( CLASS_PATH . "/Article.php" );
+function handleException( $exception ) {
+  echo "Sorry, a problem occurred. Please try later.";
+  error_log( $exception->getMessage() );
+}
+
+set_exception_handler( 'handleException' );
